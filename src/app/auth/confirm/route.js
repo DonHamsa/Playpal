1317f1a0aac57a25681be3a76399e95bc85a2180
Hamsa <hamsamuse.hm@gmail.com>
2024-12-12 16:15:02 +0000

    changed font size of marker

--- src/app/auth/confirm/route.js
+++ src/app/auth/confirm/route.js
@@ -1,28 +1,28 @@
-import { createClient } from '../../../../utils/supabase/server'
-import { redirect } from 'next/navigation'
+// import { createClient } from '../../../../utils/supabase/server'
+// import { redirect } from 'next/navigation'
 
-export async function GET(request) {
-  console.log(request)
-  const { searchParams } = new URL(request.url)
-  const token_hash = searchParams.get('token_hash')
-  const type = searchParams.get('type') // removed type casting
-  const next = searchParams.get('next') ?? '/'
+// export async function GET(request) {
+//   console.log(request)
+//   const { searchParams } = new URL(request.url)
+//   const token_hash = searchParams.get('token_hash')
+//   const type = searchParams.get('type') // removed type casting
+//   const next = searchParams.get('next') ?? '/'
 
-  if (token_hash && type) {
-    const supabase = await createClient()
+//   if (token_hash && type) {
+//     const supabase = await createClient()
 
-    const { error } = await supabase.auth.verifyOtp({
-      type,
-      token_hash,
-    })
-    if (!error) {
-      console.log(next)
-      // redirect user to specified redirect URL or root of app
-      redirect(next)
-    }
-  }
+//     const { error } = await supabase.auth.verifyOtp({
+//       type,
+//       token_hash,
+//     })
+//     if (!error) {
+//       console.log(next)
+//       // redirect user to specified redirect URL or root of app
+//       redirect(next)
+//     }
+//   }
 
-  // redirect the user to an error page with some instructions
+//   // redirect the user to an error page with some instructions
   
-  redirect('/error')
-}
+//   redirect('/error')
+// }
--- src/app/login/actions.js
+++ src/app/login/actions.js
@@ -43,7 +43,7 @@ export async function signup(formData) {
   }
 
 
-
+  
 
 
   revalidatePath("/", "layout");
