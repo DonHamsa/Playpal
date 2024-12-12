75939e91316964a9e3833652a1d5fdd62d7b5a19
Hamsa <hamsamuse.hm@gmail.com>
2024-12-12 11:40:17 +0000

    replaced funny looking url with a cleaner one

--- src/middleware.js
+++ src/middleware.js
@@ -7,7 +7,7 @@ export const config = {
 };
 
 export function middleware(request) {
-  const url= process.env.VERCEL_URL
+  const url= process.env.VERCEL_PROJECT_PRODUCTION_UR
   console.log(url)
   const referer = request.headers.get('referer');
   console.log(referer)
