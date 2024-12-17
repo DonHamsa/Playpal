65c3d534c22c36d77c0dc750e947a1e328e004a0
Hamsa <hamsamuse.hm@gmail.com>
2024-12-17 12:53:04 +0000

    reenabled middleware

--- src/middleware.js
+++ src/middleware.js
@@ -6,27 +6,27 @@ export const config = {
 
 export function middleware(request) {
 
-  // if (request.url === "http://localhost:3000/event") {
-  //   const url = process.env.VERCEL_PROJECT_PRODUCTION_URL;
-  //   const referer = request.headers.get("referer");
-  //   const allowedReferer = `https://${url}/dashboard`; // Replace with your allowed referer URL
+  if (request.url === "http://localhost:3000/event") {
+    const url = process.env.VERCEL_PROJECT_PRODUCTION_URL;
+    const referer = request.headers.get("referer");
+    const allowedReferer = `https://${url}/dashboard`; // Replace with your allowed referer URL
 
-  //   if (referer && referer.startsWith(allowedReferer)) {
-  //     return NextResponse.next(); // Allow the request
-  //   } else {
-  //     return NextResponse.redirect(new URL("/dashboard", request.url)); // Redirect to the homepage if referer doesn't match
-  //   }
-  // }
+    if (referer && referer.startsWith(allowedReferer)) {
+      return NextResponse.next(); // Allow the request
+    } else {
+      return NextResponse.redirect(new URL("/dashboard", request.url)); // Redirect to the homepage if referer doesn't match
+    }
+  }
 
-  // const { searchParams } = request.nextUrl;
-  // const message = searchParams.get("message");  
+  const { searchParams } = request.nextUrl;
+  const message = searchParams.get("message");  
 
-  // const headers = new Headers(request.headers);
-  // if  (message){
-  //   headers.set("x-current-path",message)
-  // }
+  const headers = new Headers(request.headers);
+  if  (message){
+    headers.set("x-current-path",message)
+  }
 
-  // return NextResponse.next({ headers });
+  return NextResponse.next({ headers });
 
   
 }
