301cb8ac64012efc15572df7f609ee399a4f66a8
Hamsa <hamsamuse.hm@gmail.com>
2024-12-18 11:40:17 +0000

    more more and more

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,15 @@
 import { NextResponse } from "next/server";
 
-export const config = {};
+export const config = {matcher: [
+  /*
+   * Match all request paths except for the ones starting with:
+   * - api (API routes)
+   * - _next/static (static files)
+   * - _next/image (image optimization files)
+   * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+   */
+  '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+],};
 
 export function middleware(request) {
   // console.log(process.env.VERCEL_PROJECT_PRODUCTION_URL)
