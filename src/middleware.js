7eeb51a6383261d70fe7ebf250478d63baa9cce4
Hamsa <hamsamuse.hm@gmail.com>
2024-12-18 11:37:41 +0000

    again

--- src/middleware.js
+++ src/middleware.js
@@ -5,6 +5,7 @@ export const config = {};
 export function middleware(request) {
   // console.log(process.env.VERCEL_PROJECT_PRODUCTION_URL)
   // console.log(request.url)
+  console.log('reber')
 
   if (
     request.url === `http://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/event`
