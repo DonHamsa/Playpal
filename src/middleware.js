90f958426fb447914d7ba4a97ea8202ebfbab9bd
Hamsa <hamsamuse.hm@gmail.com>
2024-12-18 11:27:48 +0000

    attempt 4

--- src/middleware.js
+++ src/middleware.js
@@ -4,6 +4,7 @@ export const config = {};
 
 export function middleware(request) {
   console.log(process.env.VERCEL_PROJECT_PRODUCTION_URL)
+  console.log(request.url)
 
   if (
     request.url === `http://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/event`
